eeb5f5758b68a2bd022ad06f9f2e9e03fd4d8f68
ChangHyunKim <hot9998@naver.com>
2021-05-22 23:32:47 +0900

    Apply grid style in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import reset from "styled-reset";
 import { createGlobalStyle } from "styled-components";
 import StyledComponentsLayout from "./components/StyledComponentsLayout";
 import EmotionLayout from "./components/EmotionLayout";
+import { css } from "@emotion/css";
 
 const GlobalStyle = createGlobalStyle`
 ${reset}
 * {
   box-sizing: border-box;
+}
+html, body, div#root {
+  width: 100%;
+  height: 100%;
 }
 `;
 
@@ -14,12 +19,19 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <div className="App">
+      <div className={appStyle}>
         <StyledComponentsLayout />
         <EmotionLayout />
       </div>
     </>
   );
 }
+
+const appStyle = css`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 100%;
+  height: 100%;
+`;
 
 export default App;
